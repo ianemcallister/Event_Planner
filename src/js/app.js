7fe4819cbcd0402dada1faebaaea11e7c85cdd0b
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2015-10-03 16:34:53 +0900

    jssor-slider up and running in angular

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 // MODULE
-var myApp = angular.module('jessicaCabral', ['ngRoute', 'ui']);
+var myApp = angular.module('jessicaCabral', ['ngRoute', 'ui', "dang-jssor"]);
 
 // ROUTES
 myApp.config(function ($routeProvider) {
@@ -24,7 +24,7 @@ myApp.config(function ($routeProvider) {
         activetab: 'events'
     })
     
-    .when('/events/:eventNum/:eventTitle', {
+    .when('/events/:eventTitle', {
         templateUrl: 'src/pages/events.html',
         controller: 'eventsController',
         activetab: 'events'
@@ -73,6 +73,13 @@ myApp.controller('landingController', ['$scope', function($scope) {
 
     $scope.name = 'Landing Page';
 
+    $(".alert").hide();
+
+    $("#Login").click(function(event) {
+
+        $("#success").fadeIn();
+    });
+
 }]);
 
 myApp.controller('aboutMeController', ['$scope', function($scope) {
@@ -93,16 +100,31 @@ myApp.controller('eventsController', ['eventDataService', '$scope', '$routeParam
         $scope.vendors = eval(String("$scope.data." + $routeParams.eventTitle + ".vendors"));
         $scope.images = eval(String("$scope.data." + $routeParams.eventTitle + ".images"));
 
-        console.log($scope.testimonials);
     });
 
+    $scope.basicUsageOptions = {
+                       $AutoPlay: true
+                   };
 
+                    $scope.sliderWithArrowOptions = {
+                        $DragOrientation: 3,                            //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+                        $SlideDuration: 80,                            //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
 
-}]);	
+                        $ArrowNavigatorOptions: {                       //[Optional] Options to specify and enable arrow navigator or not
+                            $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
+                            $ChanceToShow: 2,                           //[Required] 0 Never, 1 Mouse Over, 2 Always
+                            $AutoCenter: 2,                             //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                            $Steps: 1                                   //[Optional] Steps to go for each navigation request, default value is 1
+                        }
+                    }
+
+    $scope.images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg"];
+
+}]);    
 
 myApp.controller('pressController', ['$scope', function($scope) {
 
-	$scope.name = 'Press';
+    $scope.name = 'Press';
 }]);
 
 
@@ -110,11 +132,22 @@ myApp.controller('pressController', ['$scope', function($scope) {
 // DIRECTIVES
 myApp.directive("testimonial", function() {
     return {
-        restrict: 'AECM',
-        templateUrl: 'src/directives/testimonial.html',
+        restrict: 'E',
+        templateUrl: 'src/directives/aTestimonial.html',
         replace: true,
         scope: {
             testimonialObject: "=",
+        }
+    }
+});
+
+myApp.directive("vendor", function() {
+    return {
+        restrict: 'AECM',
+        templateUrl: 'src/directives/aVendor.html',
+        replace: true,
+        scope: {
+            vendorObject: "=",
         }
     }
 });
--- src/php/api.php
+++ src/php/api.php
@@ -12,17 +12,7 @@
 				'title' => 'Angelina & Francesco\'s Wedding',
 				'testimonials' => array
 				(
-					'one' => array
-					(
-						'copy' => 'We would like to take this time to thank you for all the hard work and dedication you put into our wedding. You\'ve exceeded our expectations for making sure our day went smoothly. I have recommended you to many people for their day of wedding planner. It was a pleasure to work with you and we appreciate all you did. It was a wedding of over 500 people and you didn\'t miss a beat, from your little touches, to your suggestions. It was extremely nice to know we had nothing to worry about during our reception. Our day was amazing and we are glad we choose you to be part of it.',
-						'author' => 'Angelina & Francesco Frano'
-					),
-					'two' => array
-					(
-						'copy' => 'We would like to take this time to thank you for all the hard work and dedication you put into our wedding. You\'ve exceeded our expectations for making sure our day went smoothly. I have recommended you to many people for their day of wedding planner. It was a pleasure to work with you and we appreciate all you did. It was a wedding of over 500 people and you didn\'t miss a beat, from your little touches, to your suggestions. It was extremely nice to know we had nothing to worry about during our reception. Our day was amazing and we are glad we choose you to be part of it.',
-						'author' => 'Angelina & Francesco Frano'
-					),
-					'three' => array
+					array
 					(
 						'copy' => 'We would like to take this time to thank you for all the hard work and dedication you put into our wedding. You\'ve exceeded our expectations for making sure our day went smoothly. I have recommended you to many people for their day of wedding planner. It was a pleasure to work with you and we appreciate all you did. It was a wedding of over 500 people and you didn\'t miss a beat, from your little touches, to your suggestions. It was extremely nice to know we had nothing to worry about during our reception. Our day was amazing and we are glad we choose you to be part of it.',
 						'author' => 'Angelina & Francesco Frano'
@@ -30,16 +20,56 @@
 				),
 				'vendors' => array
 				(
-					'Reception' => 'Paramount Conference and Event Venue', 
-					'Photographer' => 'Dmitri Markine ',
-					'Videographer' => 'Goody Cambay',
-					'Florist' => 'Gatto Flowers Distributing Inc.',
-					'Limousine' => 'Luxury Coach',
-					'Entertainment' => 'S4 Sound Sensations',
-					'Entertainment' => 'Bongo B ',
-					'Entertainment' => 'Zero Gravity Circus Productions ',
-					'Decor' => 'Under Wraps Decor',
-					'Photobooth' => 'Never Forget Photo'
+					array
+					(
+						'type' => 'Reception',
+						'name' => 'Paramount Conference and Event Venue'
+					),
+					array
+					(
+						'type' => 'Photographer',
+						'name' => 'Dmitri Markine '
+					),
+					array
+					(
+						'type' => 'Videographer',
+						'name' => 'Goody Cambay'
+					),
+					array
+					(
+						'type' => 'Florist',
+						'name' => 'Gatto Flowers Distributing Inc.'
+					),
+					array
+					(
+						'type' => 'Limousine',
+						'name' => 'Luxury Coach'
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'S4 Sound Sensations'
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'Bongo B'
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'Zero Gravity Circus Productions '
+					),
+					array
+					(
+						'type' => 'Decor',
+						'name' => 'Under Wraps Decor'
+					),
+					array
+					(
+						'type' => 'Photobooth',
+						'name' => 'Never Forget Photo'
+					)
 				),
 				'images' => array
 				(
@@ -52,24 +82,64 @@
 				'title' => 'Chelsea & Keith\'s Wedding',
 				'testimonials' => array
 				(
-					'text' => array
+					array
 					(
-						'copy' => 'Who would have thought that going to visit our hall we would walk out saying \"we need to hire them!\" Seeing the work they did and speaking for only 10 minutes I was very well assured that Serendipity would be a great choice! Hiring Serendipity & Co was the best decision my husband and I made. It gave us a peace of mind that on the day of we would not have to worry about anything except enjoy our wedding! Jessica was amazing from start to finish. Our appointments were very well organized and we left every time reassured that everything will be perfect! Anytime I had a question, Jessica was only one text away. Whether it be an idea I had or a question on how something would look, I got a very professional and honest answer within minutes! The day of our wedding, Jessica and her assistant Natalie did an amazing job with everything! They were very organized and professional throughout the whole night and we got many compliments on their great work. Working with Jessica for a year prior to our wedding she was able to know exactly how we envisioned our wedding to be just by getting to know us. She took our vision and created a hall more beautiful than we could have ever imagined! Thanks to Jessica and Natalie our wedding was truly a dream come true! ',
+						'copy' => 'Who would have thought that going to visit our hall we would walk out saying "we need to hire them!" Seeing the work they did and speaking for only 10 minutes I was very well assured that Serendipity would be a great choice! Hiring Serendipity & Co was the best decision my husband and I made. It gave us a peace of mind that on the day of we would not have to worry about anything except enjoy our wedding! Jessica was amazing from start to finish. Our appointments were very well organized and we left every time reassured that everything will be perfect! Anytime I had a question, Jessica was only one text away. Whether it be an idea I had or a question on how something would look, I got a very professional and honest answer within minutes! The day of our wedding, Jessica and her assistant Natalie did an amazing job with everything! They were very organized and professional throughout the whole night and we got many compliments on their great work. Working with Jessica for a year prior to our wedding she was able to know exactly how we envisioned our wedding to be just by getting to know us. She took our vision and created a hall more beautiful than we could have ever imagined! Thanks to Jessica and Natalie our wedding was truly a dream come true! ',
 						'author' => 'Chelsea & Keith DaSilva'
 					)
 				),
 				'vendors' => array
 				(
-					'Reception' => 'Renaissance by the Creek', 
-					'Photographer' => 'Blue Monkey Pictures Inc. ',
-					'Videographer' => 'Derek Rucas',
-					'Florist' => 'Precious Flowers N\' Things',
-					'Limousine' => 'Privately Yours',
-					'Entertainment' => 'BB Blanc',
-					'Entertainment' => 'Dance Migration',
-					'Decor' => 'Elegante Decor  ',
-					'Cake' => 'La Crème Cakes',
-					'Hair and Make-up' => 'Lore Dana\'s Hair Studio & Spa'
+					array
+					(
+						'type' => 'Reception',
+						'name' => 'Renaissance by the Creek'
+					),
+					array
+					(
+						'type' => 'Photographer',
+						'name' => 'Blue Monkey Pictures Inc. '
+					),
+					array
+					(
+						'type' => 'Videographer',
+						'name' => 'Derek Rucas'
+					),
+					array
+					(
+						'type' => 'Florist',
+						'name' => "Precious Flowers N' Things"
+					),
+					array
+					(
+						'type' => 'Limousine',
+						'name' => 'Privately Yours'
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'BB Blanc'
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'Dance Migration'
+					),
+					array
+					(
+						'type' => 'Decor',
+						'name' => 'Elegante Decor'
+					),
+					array
+					(
+						'type' => 'Cake',
+						'name' => 'La Crème Cakes'
+					),
+					array
+					(
+						'type' => 'Hair and Make-up',
+						'name' => "Lore Dana's Hair Studio & Spa"
+					)
 				),
 				'images' => array
 				(
@@ -79,11 +149,128 @@
 			),
 		'Sabrina_and_Felicio' => array
 			(
-				'title' => 'Sabrina & Felicio\'s Wedding'
+				'title' => 'Sabrina & Felicio\'s Wedding',
+				'testimonials' => array
+				(
+					array
+					(
+						'copy' => 'This past July marked one year that I married my husband. It was a long process of planning and preparations, but it was totally worth it, especially hiring a wedding planner. Jessica Cabral did not only coordinate my wedding, but she was there for every step I needed to get through the planning process. Everything from theme ideas, vendors, colours, décor, budget control, stationary, and just having someone to talk to, she had everything under control. She guided me on great ideas for the centerpieces, came with me to purchase the centerpieces and décor items for the reception, organized them to be all set up ready to go in the hall. She would come to every vendor appointment and was great with her ideas and specialties.',
+						'author' => 'Sabrina Medeiros'
+					)
+				),
+				'vendors' => array
+				(
+					array
+					(
+						'type' => 'Reception',
+						'name' => 'Portuguese Cultural Centre of Mississauga'
+					),
+					array
+					(
+						'type' => 'Photo and Video',
+						'name' => 'Digital Dreams Productions'
+					),
+					array
+					(
+						'type' => 'Florist',
+						'name' => 'Exotic Flowers & Gifts'
+					),
+					array
+					(
+						'type' => 'Limousine',
+						'name' => 'L.A. Limousines'
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'Dynamix Sound Solutions'
+					),
+					array
+					(
+						'type' => 'Decor',
+						'name' => "Vivian's Decor & Designs"
+					),
+					array
+					(
+						'type' => 'Cake',
+						'name' => "Diana's Dream Cakes"
+					)
+				),
+				'images' => array
+				(
+					1 => 'src/img/Sabrina_and_Felicio/01.jpg',
+					2 => 'src/img/Sabrina_and_Felicio/thumb_01.jpg',
+				)
 			),
 		'Morsel_and_James' => array
 			(
-				'title' => 'Morsel & James\'s Wedding'
+				'title' => 'Morsel & James\'s Wedding',
+				'testimonials' => array
+				(
+					array
+					(
+						'copy' => "Jessica and the team at Serendipity & Co. Inc. we're amazing with the planning and execution of our wedding. She went above and beyond our expectations and I could not have done it without her. Thanks to her we had great vendors and didn't have to worry because Jessica was 10 steps ahead! We love you guys and gave me the wedding of my dreams and kept me on budget! Thanks for putting up with James and I for 2.5 years!",
+						'author' => 'Morsel & James Rhodes'
+					)
+				),
+				'vendors' => array
+				(
+					array
+					(
+						'type' => 'Reception',
+						'name' => 'Presedante Banquet Hall'
+					),
+					array
+					(
+						'type' => 'Photographer',
+						'name' => 'Chris Liehmann'
+					),
+					array
+					(
+						'type' => 'Florist',
+						'name' => "Flowers N' Things"
+					),
+					array
+					(
+						'type' => 'Limousine',
+						'name' => 'Enchanted Limo '
+					),
+					array
+					(
+						'type' => 'Entertainment',
+						'name' => 'Good Vibrations'
+					),
+					array
+					(
+						'type' => 'Decor',
+						'name' => "Posh Styling's Inc."
+					),
+					array
+					(
+						'type' => 'Cake',
+						'name' => 'Karen Totten'
+					),
+					array
+					(
+						'type' => 'Officiate',
+						'name' => 'Jonathan Gallo'
+					),
+					array
+					(
+						'type' => 'Musicians',
+						'name' => 'Alexandar Gajic'
+					),
+					array
+					(
+						'type' => 'Photobooth',
+						'name' => 'Never Forget Photo'
+					)
+				),
+				'images' => array
+				(
+					1 => 'src/img/Sabrina_and_Felicio/01.jpg',
+					2 => 'src/img/Sabrina_and_Felicio/thumb_01.jpg',
+				)
 			)
 	);
 
